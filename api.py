d87057c430d27c7946a6ed63fb4dd46a4e7bc639
severusDude <fajarsaladin05@gmail.com>
2023-11-09 12:32:56 +0700

    add home template

--- api.py
+++ api.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def hello():
-    return "Server Activate"
+    return render_template('home.html')
+
+
+@app.route("/linear", methods=["GET", "POST"])
+def linear():
+    return render_template('linear.html')
+
+
+@app.route("/quadratic", methods=["GET", "POST"])
+def quadratic():
+    return render_template('quadratic.html')
 
 
 if __name__ == "__main__":
