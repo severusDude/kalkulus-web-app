e522ee1c1f61b67186a79ece9f20f3f238e9e547
severusDude <fajarsaladin05@gmail.com>
2023-11-17 17:41:30 +0700

    merge linear quadratic and cubic views into one

--- api.py
+++ api.py
@@ -10,196 +10,69 @@ def hello():
     return render_template('home.html')
 
 
-@app.route("/linear", methods=["GET", "POST", "PUT", "DELETE"])
-def linear():
+@app.route("/<function_type>", methods=["GET", "POST", "PUT", "DELETE"])
+def handle_functions(function_type):
+    func_session = f"{function_type}_func"
 
     if request.method == "GET":
-        if 'linear_func' not in session:
-            return render_template('calculator.html', func_mode="linear")
+        if func_session not in session:
+            return render_template('calculator.html', func_mode=function_type)
         else:
             image, func_detail = draw_multi_graph(
-                "linear", session.get('linear_func'))
-            return render_template('calculator.html', func_mode="linear", graph=image, func_detail=func_detail, func_information=session.get('linear_func'))
+                function_type, session.get(func_session))
+            return render_template('calculator.html', func_mode=function_type, graph=image, func_detail=func_detail, func_information=session.get(func_session))
 
     elif request.method == "POST":
 
-        func01_information = get_form_data(multi_query=True)
+        func_information = get_form_data(multi_query=True)
 
         # set default function options
-        func01_information.update({
+        func_information.update({
             'show': True,
             'marker': True,
             'color': 'blue'
         })
 
-        # create 'linear_func' item if it doesn't exist within session data
-        if 'linear_func' not in session:
-            session['linear_func'] = {'func_1': func01_information}
+        # create func_session item if it doesn't exist within session data
+        if func_session not in session:
+            session[func_session] = {'func_1': func_information}
 
         else:
-            session['linear_func'].update(
-                {f"func_{len(session['linear_func'])+1}": func01_information})
+            session[func_session].update(
+                {f"func_{len(session[func_session])+1}": func_information})
 
             # to make sure the updated session is saved
             session.modified = True
 
         image, func_detail = draw_multi_graph(
-            "linear", session.get('linear_func'))
+            function_type, session.get(func_session))
 
-        return redirect(url_for('linear'))
+        return redirect(url_for('handle_functions', function_type=function_type))
 
     elif request.method == "PUT":
         updated_func_information = request.get_json()
 
         # Process the updated function data received and update session data
         for func_id, func_data in updated_func_information.items():
-            session['linear_func'][func_id]['show'] = func_data['show']
-            session['linear_func'][func_id]['marker'] = func_data['marker']
-            session['linear_func'][func_id]['color'] = func_data['color']
+            session[func_session][func_id]['show'] = func_data['show']
+            session[func_session][func_id]['marker'] = func_data['marker']
+            session[func_session][func_id]['color'] = func_data['color']
 
             session.modified = True
 
         # redraw the graph
         image, func_detail = draw_multi_graph(
-            "linear", session.get('linear_func'))
+            function_type, session.get(func_session))
 
         # return need to be in form of json
         return jsonify({'image': image})
 
     elif request.method == "DELETE":
 
-        session.pop('linear_func', None)
+        session.pop(func_session, None)
         session.modified = True
 
-        return redirect(url_for('linear'), code=303)
-
-
-@app.route("/quadratic", methods=["GET", "POST", "PUT", "DELETE"])
-def quadratic():
-
-    if request.method == "GET":
-        if 'quadratic_func' not in session:
-            return render_template('calculator.html', func_mode="quadratic")
-        else:
-            image, func_detail = draw_multi_graph(
-                "quadratic", session.get('quadratic_func'))
-            return render_template('calculator.html', func_mode="quadratic", graph=image, func_detail=func_detail, func_information=session.get('quadratic_func'))
-
-    elif request.method == "POST":
-
-        func02_information = get_form_data(multi_query=True)
-
-        # set default function options
-        func02_information.update({
-            'show': True,
-            'marker': True,
-            'color': 'blue'
-        })
-
-        # create 'quadratic_func' item if it doesn't exist within session data
-        if 'quadratic_func' not in session:
-            session['quadratic_func'] = {'func_1': func02_information}
-
-        else:
-            session['quadratic_func'].update(
-                {f"func_{len(session['quadratic_func'])+1}": func02_information})
-
-            # to make sure the updated session is saved
-            session.modified = True
-
-        image, func_detail = draw_multi_graph(
-            "quadratic", session.get('quadratic_func'))
-
-        return redirect(url_for('quadratic'))
-
-    elif request.method == "PUT":
-        updated_func_information = request.get_json()
-
-        # Process the updated function data received and update session data
-        for func_id, func_data in updated_func_information.items():
-            session['quadratic_func'][func_id]['show'] = func_data['show']
-            session['quadratic_func'][func_id]['marker'] = func_data['marker']
-            session['quadratic_func'][func_id]['color'] = func_data['color']
-
-            session.modified = True
-
-        # redraw the graph
-        image, func_detail = draw_multi_graph(
-            "quadratic", session.get('quadratic_func'))
-
-        # return need to be in form of json
-        return jsonify({'image': image})
-
-    elif request.method == "DELETE":
-
-        session.pop('quadratic_func', None)
-        session.modified = True
-
-        return redirect(url_for('quadratic'), code=303)
-
-
-@app.route("/cubic", methods=["GET", "POST", "PUT", "DELETE"])
-def cubic():
-
-    if request.method == "GET":
-        if 'cubic_func' not in session:
-            return render_template('calculator.html', func_mode="cubic")
-        else:
-            image, func_detail = draw_multi_graph(
-                "cubic", session.get('cubic_func'))
-            return render_template('calculator.html', func_mode="cubic", graph=image, func_detail=func_detail, func_information=session.get('cubic_func'))
-
-    elif request.method == "POST":
-
-        func03_information = get_form_data(multi_query=True)
-
-        # set default function options
-        func03_information.update({
-            'show': True,
-            'marker': True,
-            'color': 'blue'
-        })
-
-        # create 'cubic_func' item if it doesn't exist within session data
-        if 'cubic_func' not in session:
-            session['cubic_func'] = {'func_1': func03_information}
-
-        else:
-            session['cubic_func'].update(
-                {f"func_{len(session['cubic_func'])+1}": func03_information})
-
-            # to make sure the updated session is saved
-            session.modified = True
-
-        image, func_detail = draw_multi_graph(
-            "cubic", session.get('cubic_func'))
-
-        return redirect(url_for('cubic'))
-
-    elif request.method == "PUT":
-        updated_func_information = request.get_json()
-
-        # Process the updated function data received and update session data
-        for func_id, func_data in updated_func_information.items():
-            session['cubic_func'][func_id]['show'] = func_data['show']
-            session['cubic_func'][func_id]['marker'] = func_data['marker']
-            session['cubic_func'][func_id]['color'] = func_data['color']
-
-            session.modified = True
-
-        # redraw the graph
-        image, func_detail = draw_multi_graph(
-            "cubic", session.get('cubic_func'))
-
-        # return need to be in form of json
-        return jsonify({'image': image})
-
-    elif request.method == "DELETE":
-
-        session.pop('cubic_func', None)
-        session.modified = True
-
-        return redirect(url_for('cubic'), code=303)
+        return redirect(url_for('handle_functions', function_type=function_type), code=303)
 
 
 def get_form_data(multi_query=True):
